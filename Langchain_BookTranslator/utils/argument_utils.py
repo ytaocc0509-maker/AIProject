882a5a8dcf4d734c0558c42f7e7388dd14c49e11
ytaocc0509-maker <ytaocc0509@gmail.com>
2026-05-13 23:30:41 +0800

    项目配置

---
+++ Langchain_BookTranslator/utils/argument_utils.py
@@ -0,0 +1,28 @@
+import  argparse
+
+class ArgumentUtils:
+    """
+    命令行参数解析对象
+    """
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description='书籍自动翻译器')
+        self.parser.add_argument('--config', type=str, default='config.yaml', help='项目的整体配置文件')
+        self.parser.add_argument('--model_type', type=str, default='OpenAIModel', choices=['GLMModel', 'OpenAIModel'],
+                                 help='选择OpenAI还是GLM的模型')
+        self.parser.add_argument('--model_name', type=str, help='大预言模型的名字')
+        self.parser.add_argument('--input_file', type=str, help='需要翻译的书籍所属的文件路径')
+        self.parser.add_argument('--file_format', type=str, help='翻译之后生成的文件格式')
+        self.parser.add_argument('--source_language', type=str, help='书记的原始语言')
+        self.parser.add_argument('--target_language', type=str, help='翻译之后的语言')
+
+    def parse_arg(self):
+        """
+        解析和验证命令中的参数
+        :return:
+        """
+        # 解析参数
+        args = self.parser.parse_args()
+        # 参数的验证，可以有更多
+        # if args.model_type == 'OpenAIModel' and not args.openai_model and not args.openai_api_key:
+        #     self.parser.error('当选择OpenAI后，--openai_model和--openai_api_key参数，必须要传！')
+        return args
---
+++ Langchain_BookTranslator/utils/project_config.py
@@ -0,0 +1,56 @@
+import yaml
+
+from utils.argument_utils import ArgumentUtils
+
+
+class ProjectConfig:
+    """
+    统一处理整个项目的配置，整个项目的配置对象设置为单例
+    """
+    _instance = None  # 当前这个类的实例
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(ProjectConfig, cls).__new__(cls)
+            cls._instance._config = None
+            cls._instance._args = None
+        return cls._instance
+
+    def initialize(self):
+        """
+        初始化所有的项目配置
+        :return:
+        """
+
+        # 环境变量配置的初始化
+
+        # 命令行参数配置的初始化
+
+        if self._args is None:
+            arg_utils = ArgumentUtils()
+            self._args = arg_utils.parse_arg()
+
+        # YAML 文件配置的初始化 ： 如果YAML中的配置和命令行参数冲突，以命令参数为准
+        if self._config is None:
+            with open(self._args.config, 'r') as f:
+                config = yaml.safe_load(f)
+
+            overridden_config = {  # 所有冲突的配置字典
+                key: value for key, value in vars(self._args).items()
+                if key in config and value is not None
+            }
+
+            config.update(overridden_config)  # 把命令的参数覆盖config文件里面的
+            self._config = config
+
+    def __getattr__(self, item):
+        # 当访问当前对象实例属性时自动调用
+        if self._config and item in self._config:
+            return self._config[item]
+        raise AttributeError(f'项目配置中没有一个属性：{item}')
+
+
+if __name__ == '__main__':
+    o1 = ProjectConfig()
+    o2 = ProjectConfig()
+    print(o1 is o2)
